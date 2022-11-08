aa2b856fd3441b39497e7875e683dff86eb5c96e
Greyes90 <greyes0731@gmail.com>
2022-11-07 20:54:24 -0600

    "Parameters for each class added"

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -1,1 +1,38 @@
 from pydantic import BaseModel
+import pydantic 
+
+
+
+class Book(BaseModel):  
+    id:int
+title:str
+artist:str
+issue_year:int
+number_of_pages: int
+genre:str
+price:int
+
+
+
+book = Book 
+
+class Artist(BaseModel):
+    id:int
+full_name:str
+alias:str
+country:str
+prefered_genre:str
+
+
+
+artist = Artist
+
+class Customer(BaseModel):
+    id:int
+full_name:str
+address:str 
+prefered_genre:str
+
+customer = Customer 
+
+
