679ce8a09ad2e9b27ee2c98f1fee291c8361a4b6
Greyes90 <greyes0731@gmail.com>
2022-11-07 22:06:26 -0600

    "Models created for each Class"

--- app/models/db_models.py
+++ app/models/db_models.py
@@ -13,9 +13,6 @@ genre:str
 price:int
 
 
-
-book = Book 
-
 class Artist(BaseModel):
     id:int
 full_name:str
@@ -24,15 +21,26 @@ country:str
 prefered_genre:str
 
 
-
-artist = Artist
-
 class Customer(BaseModel):
     id:int
 full_name:str
 address:str 
 prefered_genre:str
 
-customer = Customer 
 
 
+
+
+artist = Artist(id= 1, full_name= "Henry Cavil", alias="Superman", country="United Kingdom", prefered_genre="Fantasy")
+if artist.full_name == None:
+        print("Please enter the artists' full name")
+
+book = Book(id=1, title="The Hobbit", artist="J.R.R. Tolkien", issue_year=1937, number_of_pages=295, genre="Fantasy", price=1000)
+if book.title and book.artist and book.page > 0 == None:
+        print("Please enter the book title and artist")
+if book.title == None:
+        print("Please enter the book's title")
+
+customer = Customer(id=1, full_name="John Doe", address="1234 Main Street", prefered_genre="Fantasy")
+if customer.full_name == None:
+        print("Please enter your full name")
